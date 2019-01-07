41a621fcab0d824f92f2fc2b97170fe1440e3fe1
bdenizar <benjamin.denizart@free.fr>
2019-01-07 12:04:07 +0100

    Update and rename 1546861763_quote_adress.sql to 1546861763_quote_address.sql

--- sql/1546861763_quote_address.sql
+++ sql/1546861763_quote_address.sql
@@ -1,15 +1,14 @@
-CREATE TABLE quote_adress
+CREATE TABLE `quote_address`
 (
-    quote_adress_id INT(10) NOT NULL,
-    quote_id INT(10) NOT NULL,
-    customer_adress_id INT(10),
-    type TINYINT(1) NOT NULL,
-    nb INT(5) NOT NULL,
+    quote_adress_id INT(10) UNSIGNED NOT NULL,
+    quote_id INT(10) UNSIGNED NOT NULL,
+    customer_adress_id INT(10) UNSIGNED NULL,
+    address_type TINYINT(1) NOT NULL,
     street_1 VARCHAR(100) NOT NULL,
     street_2 VARCHAR(100),
-    postcode INT(5) NOT NULL,
-    city VARCHAR(30) NOT NULL,
-    country VARCHAR(30) NOT NULL,
+    postcode VARCHAR(60) NOT NULL,
+    city VARCHAR(100) NOT NULL,
+    country_id CHAR(2) NOT NULL,
     PRIMARY KEY (quote_adress_id),
     CONSTRAINT fk_quote_adress_to_quote FOREIGN KEY (quote_id) REFERENCES quote(quote_id), 
     CONSTRAINT fk_quote_adress_to_custumer_adress FOREIGN KEY (customer_adress_id) REFERENCES customer_adress(customer_adress_id)
