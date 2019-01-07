c56a8e6f9720ff03f7ef277975b5e51287b2d80c
Paul Sinnah <paulsnnh@gmail.com>
2019-01-07 10:58:59 +0100

    Add quote_item table to the project

---
+++ sql/1546855110_quote_item.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `ecommerce`.`quote_item` (
+    `quote_item_id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `quote_id` INT UNSIGNED NOT NULL,
+    `product_id` INT UNSIGNED NOT NULL,
+    `price` DECIMAL(12, 4) UNSIGNED NOT NULL,
+    `row_total` DECIMAL(12, 4) UNSIGNED NOT NULL,
+    `qty` INT UNSIGNED NOT NULL,
+    `sku` VARCHAR(64) NOT NULL,
+    `ean` VARCHAR(13) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `tax_amount` DECIMAL(12, 4) NOT NULL,
+    `created_at` TIMESTAMP NOT NULL,
+    `updated_at` TIMESTAMP NOT NULL,
+    `discount_amount` DECIMAL(12, 4) NOT NULL,
+    FOREIGN KEY (`quote_id`) REFERENCES quote(`quote_id`),
+    FOREIGN KEY (`product_id`) REFERENCES product(`product_id`)
+) ENGINE = InnoDB DEFAULT CHARSET=utf8;
