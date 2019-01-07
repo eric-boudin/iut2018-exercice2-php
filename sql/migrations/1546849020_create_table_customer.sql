31e6feffb42b3ce368e261e07af1d938b96ab292
Benjamin Denizart <denizart.benjamin@gmail.com>
2019-01-07 16:15:04 +0100

    Fix sql & add lot of seeds for customer table

--- sql/migrations/1546849020_create_table_customer.sql
+++ sql/migrations/1546849020_create_table_customer.sql
@@ -5,7 +5,7 @@ CREATE TABLE `customer` (
   `firstname` varchar(255) NOT NULL,
   `lastname` varchar(255) NOT NULL,
   `gender` tinyint(1) DEFAULT NULL,
-  `dob` timestamp NULL DEFAULT NULL,
+  `dob` date NULL DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`customer_id`),
