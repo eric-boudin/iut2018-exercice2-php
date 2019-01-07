6c2c2a7d0a304b112bb8b944de29161dd38203d8
SINNAH Paul <sp714099@iutnice.unice.fr>
2019-01-07 11:03:26 +0100

    Indent quote_item file correctly

--- sql/1546855110_quote_item.sql
+++ sql/1546855110_quote_item.sql
@@ -12,8 +12,8 @@ CREATE TABLE IF NOT EXISTS `quote_item` (
     `created_at` TIMESTAMP NOT NULL,
     `updated_at` TIMESTAMP NOT NULL,
     `discount_amount` DECIMAL(12, 4) UNSIGNED NOT NULL,
-	`shipping_amount` DECIMAL(12, 4) UNSIGNED NOT NULL;
-	`shipping_amount_include_tax` DECIMAL(12, 4) UNSIGNED NOT NULL;
+    `shipping_amount` DECIMAL(12, 4) UNSIGNED NOT NULL;
+    `shipping_amount_include_tax` DECIMAL(12, 4) UNSIGNED NOT NULL;
     FOREIGN KEY (`quote_id`) REFERENCES quote(`quote_id`),
     FOREIGN KEY (`product_id`) REFERENCES product(`product_id`)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8;
